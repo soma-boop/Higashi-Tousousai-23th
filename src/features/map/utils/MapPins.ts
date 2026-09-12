b8f2e8a827ed33eefb467bc1d5f82c8596b212a8
soma-boop <xidong0116@gmail.com>
2026-09-12 21:07:01 +0900

    Update festival maps and booth info

--- src/features/map/utils/MapPins.ts
+++ src/features/map/utils/MapPins.ts
@@ -8,7 +8,7 @@ export const MapPins: Record<string, { mapId: number; x: number; y: number }> = 
 
 // category, title, map image path
 export const mapList = [
-  { category: "校舎", title: "1F / 中庭", src: "/img/1.map.png" },
-  { category: "校舎", title: "2F / 3F", src: "/img/2to3.map.png" },
-  { category: "全体", title: "全体図", src: "/img/all.map.png" },
+  { category: "校舎", title: "1F / 中庭", src: "/img/map/1stmap.png" },
+  { category: "校舎", title: "2F / 3F", src: "/img/map/2and3map.png" },
+  { category: "全体", title: "全体図", src: "/img/map/allmap.png" },
 ];
